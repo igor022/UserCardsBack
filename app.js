8a15849ab61bcb2cefc0c32e4b3af882370c93fc
igor022 <litvinigor022@gmail.com>
2020-10-08 16:45:20 +0300

    adding User Schema

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
+
 
 const app = express();
 const port = process.env.PORT || 8080;
-const userRoutes = require('./routes/userRoutes');
 
 const dbUri = `mongodb+srv://dbUser:${process.env.DB_PASSWORD}@cardscluster.rvd7d.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+mongoose.connect(dbUri, {useNewUrlParser: true, useUnifiedTopology: true});
 app.listen(port);
 
 app.get('/', (req, res) => {

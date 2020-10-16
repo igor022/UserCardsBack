231c7f3b4a58483788580716e1b58013059ade53
igor022 <litvinigor022@gmail.com>
2020-10-16 14:48:11 +0300

    add project routes

---
+++ controllers/projectController.js
@@ -0,0 +1,56 @@
+const Project = require('../models/Project');
+
+
+const getProjects = async (req, res) => {
+  try {
+    const results = await Project.find().sort({ createdAt: -1 });
+    
+    res.send(results);
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+const addProject = async (req, res) => {
+  try {
+    const { project } = req.body;
+    const addedProject = new Project(project);
+    await addedProject.save();
+    
+    res.send(addedProject);
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+const editProject = async (req, res) => {
+  try {
+    const { project } = req.body;
+    await Project.updateOne({ _id: project._id }, project);
+    const updatedProject = await Project.findOne({ _id: project._id});
+    
+    res.send(updatedProject);
+  } catch(err) {
+    throw(err);
+  }
+}
+
+const deleteProject = async (req, res) => {
+  try {
+    // delete user
+    const id = req.body.id;
+    const project = await Project.findByIdAndDelete(id);
+    
+    res.send(project);
+
+  } catch (err) {
+    throw(err);
+  }
+}
+
+module.exports = {
+  getProjects,
+  addProject,
+  editProject,
+  deleteProject
+}
